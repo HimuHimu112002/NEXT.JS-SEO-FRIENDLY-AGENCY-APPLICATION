05ab302554e54f880f8b624aa7bebac8bb9d1107
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-11-22 19:50:55 +0600

    next

--- src/app/Api.js
+++ src/app/Api.js
@@ -28,5 +28,9 @@ export async function getBrandLogo(){
         throw new Error("BrandList Calling Fail");
     }
     return res.json();
- }
+}
  
+// export function KI(){
+//     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+// }
+
